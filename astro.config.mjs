b1b63d98f67b74d651a4d8bfe3196a1ad894f51a
firthunandz <fernandonchiesa@gmail.com>
2025-01-17 13:25:59 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+const LOCALHOST_URL = 'http://localhost:4321/';
+const LIVE_URL = 'https://firthunandz.github.io';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: LIVE_URL,
+  integrations: [tailwind()],
 });
