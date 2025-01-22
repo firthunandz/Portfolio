2a3a8c6ba916d99eabc7ea49fb523b48f0b8aa04
firthunandz <fernandonchiesa@gmail.com>
2025-01-22 16:31:30 -0300

    updated projects data and div

--- src/data/Education.js
+++ src/data/Education.js
@@ -1,18 +1,21 @@
 const courses = [
   {
     date: 'Septiembre 2024',
-    title: 'Front end Talento tech',
-    description: 'adasdlksnakdlaskldasdaskldjaskldjasldklasdksadas',
+    title: 'Front end developer',
+    institution: 'Talento-tech',
+    description: 'El proyecto era una página web responsive a elección en HTML con CSS y JS',
   },
   {
     date: 'Febrero 2024',
-    title: 'React codo a codo',
-    description: 'adasdlksnakdlaskldasdaskldjaskldjasldklasdksadas',
+    title: 'React JS',
+    institution: 'Codo a codo',
+    description: 'Especialización en React. El proyecto final era una web utilizando fetch para utilizar una API a elección',
   },
   {
     date: 'Agosto 2023',
-    title: 'Full stack codo a codo',
-    description: 'adasdlksnakdlaskldasdaskldjaskldjasldklasdksadas',
+    title: 'Full stack NodeJS',
+    institution: 'Codo a codo',
+    description: 'El proyecto era un e-commerce de Funko-pop utilizando Express para el backend, SQL y Sequelize para base de datos y HTML, CSS y EJS para el frontend',
   },
 ];
 
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -2,30 +2,35 @@ import tags from '../data/Tags.js';
 
 const projects = [
   {
+    img: './images/portfolioscreen.jpg',
     title: 'Portafolio',
     github: 'https://github.com/firthunandz/Portfolio',
-    description: 'Portafolio hecho en Astro',
+    description: 'Portafolio ersonal hecho en Astro',
     tags: [tags.HTML, tags.CSS, tags.JavaScript, tags.Astro]
   },
   {
+    img: './images/fermentoswebscreen.jpg',
     title: 'Fermentos Web',
     github: 'https://github.com/firthunandz/Fermentos-web',
     description: 'Web para mi emprendimiento "Fer-mentos"',
     tags: [tags.HTML, tags.CSS, tags.JavaScript]
   },
   {
+    img: './images/neonsnakescreen.jpg',
     title: 'Neon Snake Game',
     github: 'https://github.com/firthunandz/NeonSnake',
-    description: 'Snakegame',
+    description: 'Snake game hecho en React',
     tags: [tags.HTML, tags.CSS, tags.JavaScript, tags.React]
   },
   {
+    img: './images/pokeclickscreen.jpg',
     title: 'Poke Click',
     github: 'https://github.com/firthunandz/PokeClick',
-    description: 'Idle clicker game',
+    description: 'Idle clicker game using PokeAPI',
     tags: [tags.HTML, tags.CSS, tags.JavaScript]
   },
   {
+    img: './images/funkopopscreen.jpg',
     title: 'Funko-POP e-commerce',
     github: 'https://github.com/firthunandz/CaC-finalProyect',
     description: 'Proyecto final para el bootcamp Codo a codo',
